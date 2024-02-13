c68b33ee08745ab08198f1901e94700434926af6
rizky <rizky@Rizky>
2024-02-13 10:28:46 +0700

    Update TC

--- Scripts/Checkout/Script1707794763935.groovy
+++ Scripts/Checkout/Script1707794763935.groovy
@@ -20,13 +20,23 @@ import org.openqa.selenium.Keys as Keys
 Mobile.startApplication('C:\\Users\\rizky\\Documents\\Android-Solodroid_E-CommerceApp Demo_3.2.0.apk\\Solodroid_E-CommerceApp Demo_3.2.0.apk', 
     true)
 
-Mobile.tap(findTestObject('Object Repository/Add to Chart/android.widget.ImageView'), 0)
+Mobile.tap(findTestObject('Object Repository/Checkout/android.widget.ImageButton'), 0)
 
-Mobile.tap(findTestObject('Object Repository/Add to Chart/android.widget.Button - ADD TO CART'), 0)
+Mobile.tap(findTestObject('Object Repository/Checkout/android.widget.Button - CHECKOUT'), 0)
 
-Mobile.setText(findTestObject('Object Repository/Add to Chart/android.widget.EditText'), '2', 0)
+Mobile.setText(findTestObject('Object Repository/Checkout/android.widget.EditText - Your Name'), 'Fariz', 0)
 
-Mobile.tap(findTestObject('Object Repository/Add to Chart/android.widget.Button - ADD'), 0)
+Mobile.setText(findTestObject('Object Repository/Checkout/android.widget.EditText - your.emailgmail.com'), 'Fariz@gmail.com', 
+    0)
+
+Mobile.setText(findTestObject('Object Repository/Checkout/android.widget.EditText - Your Address'), 'Rukita sama kamu', 
+    0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Object Repository/Checkout/android.widget.Button - PROCESS CHECKOUT'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Checkout/android.widget.Button - YES'), 0)
 
 //Mobile.closeApplication()
 
